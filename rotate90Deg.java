e2e3031b9542632ce33bf8e3ea970da4c5b28f62
Vishakha Kalaskar <vishakhakalaskar986@gmail.com>
2026-02-12 10:55:44 +0530

    Solution for Rotate Matrix To 90 Degree

---
+++ rotate90Deg.java
@@ -0,0 +1,53 @@
+class rotate90Deg {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        // (Swap matrix[i][j] with matrix[j][i])
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original matrix:");
+        printMatrix(matrix);
+
+        rotate90Deg obj = new rotate90Deg();
+        obj.rotate(matrix);
+
+        System.out.println("Rotated matrix (90 degrees clockwise):");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
